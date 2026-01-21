5a729ccd41c1a4509258c1111ac0a4018fd1529e
prasad1604 <prasad.kotian506@gmail.com>
2026-01-21 12:10:25 +0530

    Changed base url

--- moneymanager_frontend/src/util/apiEndpoints.js
+++ moneymanager_frontend/src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://money-manager-uw9r.onrender.com/api/v1.0";
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://money-manager-uw9r.onrender.com/api/v1.0";
+//export const BASE_URL = "http://localhost:8080/api/v1.0";
 const CLOUDINARY_CLOUD_NAME = "duvixehjw";
 
 export const API_ENDPOINTS = {
